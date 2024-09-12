5d3851e964f596ca3f10fefdf3a88e5db2bc5814
Jort-png <Jort@Lievense.nl>
2024-09-12 11:05:01 +0200

    1ste commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,8 @@
+<x-layouts.app>
+    
+<h1>Over ons</h1>
+
+<div class="wrapper">
+<p>Wij zijn een jong en dynamisch team dat zich richt op het ontwikkelen van innovatieve weboplossingen. Ons doel is om gebruikersvriendelijke en efficiënte websites te bouwen voor bedrijven van elke omvang. Onze passie ligt in het verbeteren van de digitale ervaringen van onze klanten.</p>
+</div>
+</x-layouts.app>
---
+++ resources/views/components/footer.php
@@ -0,0 +1,6 @@
+<footer style="background-color: #f1f1f1; padding: 20px; text-align: center; position: relative;">
+    <p>&copy; 2024 - Jouw Bedrijfsnaam. Alle rechten voorbehouden.</p>
+
+<div style="position: absolute; bottom: 10px; right: 10px; opacity: 0.1; font-size: 50px;">
+        &#169; Curio Lievense
+    </div>
